01ca391128b5100bc39a1e66494acb9fc2bceaa3
Taimoor Arshad <taimoor.1998@gmail.com>
2025-03-09 00:38:17 -0700

    add waiting for db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { PerformanceService } from './services/performance.service';
 import { PerformanceResult } from './shared/models/performance-result';
 import { CommonModule } from '@angular/common';
+import { interval, switchMap, takeWhile } from 'rxjs';
 
 @Component({
     selector: 'app-root',
@@ -10,15 +11,37 @@ import { CommonModule } from '@angular/common';
     imports: [FormsModule, CommonModule],
     templateUrl: './app.component.html'
 })
+
 export class AppComponent implements OnInit {
-    results: any[] = [];
+    results: PerformanceResult[] = [];
     url = '';
     latestResult: PerformanceResult | null = null;
+    isLoading = true; // Track database availability
+    loadingMessage = 'Waiting for database response...';
 
     constructor(private performanceService: PerformanceService) { }
 
     ngOnInit() {
-        this.getResults();
+        this.pollDatabase();
+    }
+
+    pollDatabase() {
+         interval(1000).pipe(
+            switchMap(() => this.performanceService.getResults()),
+            takeWhile((data, index) => {
+                if (data) {
+                    this.results = data;
+                    this.isLoading = false;
+                    this.loadingMessage = '';
+                    return false;
+                }
+                this.loadingMessage = `Waiting for database response... (Attempt ${index + 1})`;
+                return true;
+            }, true)
+         )
+         .subscribe({
+            error: (err) =>  console.error('Error fetching results:', err)
+         });    
     }
 
     getResults() {
