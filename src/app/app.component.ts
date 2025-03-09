f22f13a0341e25712aa51b7fd69ef2994123bc52
Taimoor Arshad <taimoor.1998@gmail.com>
2025-03-08 17:25:47 -0700

    change html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { PerformanceService } from './services/performance.service';
+import { PerformanceResult } from './shared/models/performance-result';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [FormsModule],
+    imports: [FormsModule, CommonModule],
     templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
     results: any[] = [];
     url = '';
+    latestResult: PerformanceResult | null = null;
 
     constructor(private performanceService: PerformanceService) { }
 
@@ -19,13 +22,19 @@ export class AppComponent implements OnInit {
     }
 
     getResults() {
-        this.performanceService.getResults().subscribe(data => this.results = data);
+        this.performanceService.getResults().subscribe({
+            next: (data) => this.results = data,
+            error: (err) => console.error('Error fetching results:', err)
+        });
     }
-
     analyze() {
-        this.performanceService.analyzeWebsite(this.url).subscribe(() => {
-            this.url = '';
-            this.getResults();
+        this.performanceService.analyzeWebsite(this.url).subscribe({
+            next: (result) => {
+                this.latestResult = result; // Store latest result
+                this.url = '';
+                this.getResults(); // Refresh full list
+            },
+            error: (err) => console.error('Analyze error:', err)
         });
     }
 }
